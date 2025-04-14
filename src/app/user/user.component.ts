f4391a5cacd680f55686a3f51daadd578959f8ae
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-04-14 12:07:05 +0200

    feat: update user component to bind image path and display selected user avatar

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,4 +12,8 @@ const randomIndex = Math.floor(Math.random() * USERS.length);
 })
 export class UserComponent {
   selectedUser = USERS[randomIndex];
+
+  get imageUrl() {
+    return `assets/users/${this.selectedUser.avatar}`;
+  }
 }
