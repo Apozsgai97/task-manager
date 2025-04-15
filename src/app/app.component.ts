01bb2d24d1567dc75a74e957f51db61e7b127a89
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-04-15 11:01:41 +0200

    feat: add TasksComponent to display selected user tasks and update app structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,21 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { USERS } from './users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = USERS;
+  selectedUserName!: string;
+
   onSelectUser(id: string) {
-    console.log('Selected user id:', id);
+    const user =  this.users.find((user) => user.id === id)
+    this.selectedUserName = user!.name
   }
 }
